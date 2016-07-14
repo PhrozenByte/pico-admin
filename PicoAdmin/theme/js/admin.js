4beb6548fae1785a95fb989932ff9c848a1f9d04
Daniel Rudolf <github.com@daniel-rudolf.de>
2016-07-15 01:32:49 +0200

    Add loading bar

--- PicoAdmin/theme/js/admin.js
+++ PicoAdmin/theme/js/admin.js
@@ -12,6 +12,19 @@ PicoAdmin.prototype.ajax = function (module, action, payload, options) {
         delete options.postData.auth_client_token;
     }
 
+    this.showLoading();
+
+    var completeCallback = options.complete;
+    options.complete = (function (xhr, statusText, response) {
+        this.hideLoading();
+
+        if (completeCallback) {
+            completeCallback(xhr, statusText, response);
+        }
+    }).bind(this);
+
+    // TODO: globally catch errors and print them somewhere
+
     return utils.ajax(this.getUrl(module, action, payload), options);
 };
 
@@ -28,6 +41,59 @@ PicoAdmin.prototype.getUrl = function (module, action, payload) {
     }
 
     return url;
+};
+
+PicoAdmin.prototype.showLoading = function () {
+    var loading = document.getElementById('loading'),
+        animateProgress = function () { loading.style.width = (50 + Math.random() * 30) + '%'; };
+
+    if (loading) {
+        loading.dataset.requests = parseInt(loading.dataset.requests) + 1;
+
+        if (loading.classList.contains('finish')) {
+            window.clearTimeout(loading.dataset.timeout);
+            delete loading.dataset.timeout;
+
+            loading.classList.remove('finish');
+
+            loading.classList.add('wait');
+            animateProgress();
+        } else if (!loading.classList.contains('wait')) {
+            loading.classList.add('wait');
+            animateProgress();
+        }
+    } else {
+        loading = utils.parse('<div id="loading" class="wait"><div class="glow"></div></div>');
+        loading.dataset.requests = 1;
+
+        window.requestAnimationFrame(function () {
+            document.body.appendChild(loading);
+            window.requestAnimationFrame(animateProgress);
+        });
+    }
+};
+
+PicoAdmin.prototype.hideLoading = function () {
+    var loading = document.getElementById('loading');
+    if (loading) {
+        var requestCount = parseInt(loading.dataset.requests);
+        loading.dataset.requests = requestCount - 1;
+
+        if (requestCount == 1) {
+            loading.classList.remove('wait');
+            loading.classList.add('finish');
+            loading.style.width = null;
+
+            loading.dataset.timeout = window.setTimeout(function () {
+                loading.classList.remove('finish');
+                delete loading.dataset.timeout;
+            }, 800);
+
+            return true;
+        }
+    }
+
+    return false;
 };
 
 PicoAdmin.prototype.getAuthToken = function () {
